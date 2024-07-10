8a4ea227c0555202f1ca8c2833ab6ca77b9a2b11
Poleric <nixonleongkb@gmail.com>
2024-07-10 23:19:39 +0800

    add purchasePrice and salePrice

--- src/main/java/com/lavacorp/item/Item.java
+++ src/main/java/com/lavacorp/item/Item.java
@@ -3,13 +3,15 @@ package com.lavacorp.item;
 public class Item {
     public String name;
     public int id;
-    public double price;
+    public double purchasePrice;
+    public double salePrice;
     public ItemType type;
 
-    public Item(String name, int id, double price, ItemType type) {
+    public Item(String name, int id, double purchasePrice, double salePrice, ItemType type) {
         this.name = name;
         this.id = id;
-        this.price = price;
+        this.purchasePrice = purchasePrice;
+        this.salePrice = salePrice;
         this.type = type;
     }
 }
