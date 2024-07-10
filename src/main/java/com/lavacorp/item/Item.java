c9a3981b76141921e686aa3fab8d98dca882d350
Poleric <nixonleongkb@gmail.com>
2024-07-10 23:12:43 +0800

    set public to attributes

--- src/main/java/com/lavacorp/item/Item.java
+++ src/main/java/com/lavacorp/item/Item.java
@@ -1,11 +1,10 @@
 package com.lavacorp.item;
 
 public class Item {
-    String name;
-    int id;
-
-    double price;
-    ItemType type;
+    public String name;
+    public int id;
+    public double price;
+    public ItemType type;
 
     public Item(String name, int id, double price, ItemType type) {
         this.name = name;
