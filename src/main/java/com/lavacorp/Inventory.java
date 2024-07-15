adbe7e2000b245418f4a1be00d655e9371b38fe1
Poleric <nixonleongkb@gmail.com>
2024-07-16 00:30:40 +0800

    init things

--- src/main/java/com/lavacorp/Inventory.java
+++ src/main/java/com/lavacorp/Inventory.java
@@ -1,10 +1,13 @@
 package com.lavacorp;
 
 import com.lavacorp.database.Database;
+import com.lavacorp.modules.storage.DatabaseStorage;
 import com.lavacorp.modules.storage.Storage;
 import com.lavacorp.modules.transaction.TransactionLogger;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
+import java.util.ArrayList;
 
 
 public class Inventory {
@@ -13,5 +16,19 @@ public class Inventory {
     protected TransactionLogger transactionLogger;
     protected final static Logger LOGGER = LogManager.getLogger(Inventory.class);
 
+    public Inventory() {
+        database = Database.getInstance();
+        database.connect("inventory.db");
 
+        storage = new DatabaseStorage();
+//        transactionLogger = new TransactionLogger();
+    }
+
+    public void addItem(int itemId, int quantity) {
+        storage.addItem(itemId, quantity);
+    }
+
+    public void removeItem(int itemId, int quantity) {
+        storage.removeItem(itemId, quantity);
+    }
 }
