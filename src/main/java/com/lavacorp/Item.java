f71c9b5c7952280c66253e931002b45f76de3c44
Poleric <nixonleongkb@gmail.com>
2024-07-15 21:53:41 +0800

    simplify item

---
+++ src/main/java/com/lavacorp/Item.java
@@ -0,0 +1,11 @@
+package com.lavacorp;
+
+public class Item {
+    public int id;
+    public String name;
+
+    public Item(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+}
