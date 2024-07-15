cc2a1f80027282ea91e6b195a4b2505dd0e715bb
Poleric <nixonleongkb@gmail.com>
2024-07-16 00:01:19 +0800

    redesign inventory

---
+++ src/main/java/com/lavacorp/Inventory.java
@@ -0,0 +1,17 @@
+package com.lavacorp;
+
+import com.lavacorp.database.Database;
+import com.lavacorp.modules.storage.Storage;
+import com.lavacorp.modules.transaction_logger.TransactionLogger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+public class Inventory {
+    protected Database database;
+    protected Storage storage;
+    protected TransactionLogger transactionLogger;
+    protected final static Logger LOGGER = LogManager.getLogger(Inventory.class);
+
+
+}
