cba3ed3e17b47c41cfcfa305b09412efb3d3f2e0
Poleric <nixonleongkb@gmail.com>
2024-07-18 16:38:21 +0800

    remove code

--- src/main/java/com/lavacorp/Inventory.java
+++ src/main/java/com/lavacorp/Inventory.java
@@ -1,13 +1,11 @@
 package com.lavacorp;
 
 import com.lavacorp.database.Database;
-import com.lavacorp.modules.storage.Storage;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 
 public class Inventory {
-    protected Storage storage;
     protected final static Logger LOGGER = LogManager.getLogger(Inventory.class);
 
     public Inventory() {
@@ -16,11 +14,4 @@ public class Inventory {
 
     }
 
-    public void addItem(int itemId, int quantity) {
-        storage.addItem(itemId, quantity);
-    }
-
-    public void removeItem(int itemId, int quantity) {
-        storage.removeItem(itemId, quantity);
-    }
 }
