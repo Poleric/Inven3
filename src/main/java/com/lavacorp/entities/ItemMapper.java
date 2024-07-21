ccf98847a7d1f14734d3d03def4aa991cc656998
Poleric <nixonleongkb@gmail.com>
2024-07-21 21:54:42 +0800

    item mapper

---
+++ src/main/java/com/lavacorp/entities/ItemMapper.java
@@ -0,0 +1,20 @@
+package com.lavacorp.entities;
+
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ItemMapper implements RowMapper<Item> {
+
+    @Override
+    public Item map(ResultSet rs, StatementContext ctx) throws SQLException {
+        return new Item(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("description"),
+                rs.getDouble("base_price")
+        );
+    }
+}
