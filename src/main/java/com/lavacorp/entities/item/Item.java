d12c1b2186499a2f823c23d15e55b2cf0bd9e5ad
Poleric <nixonleongkb@gmail.com>
2024-07-22 20:32:07 +0800

    wip remapped item

--- src/main/java/com/lavacorp/entities/item/Item.java
+++ src/main/java/com/lavacorp/entities/item/Item.java
@@ -2,7 +2,9 @@ package com.lavacorp.entities.item;
 
 import com.lavacorp.entities.item.category.Category;
 import com.lavacorp.entities.tag.Tag;
-import org.jetbrains.annotations.NotNull;
+import lombok.Data;
+import org.jdbi.v3.core.mapper.Nested;
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
 import org.jetbrains.annotations.Nullable;
 
 import javax.measure.Unit;
@@ -10,70 +12,25 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
 public class Item implements Serializable {
-    private int id;
+    @ColumnName("id")
+    private final int id;
+    @ColumnName("name")
     private String name;
-    @Nullable private String description;
+    @ColumnName("description") @Nullable
+    private String description;
+    @ColumnName("base_price")
     private double basePrice;
+    @ColumnName("created_at") @Nullable
+    private LocalDateTime createdAt;
+    @ColumnName("last_update_at") @Nullable
+    private LocalDateTime lastUpdatedAt;
+    @ColumnName("category") @Nested @Nullable
+    private Category category;
 
-    @Nullable private LocalDateTime creationDate;
-    @Nullable private LocalDateTime updatedDate;
-    @Nullable private Category category;
-    @Nullable private Unit<?> unit;
-    @Nullable private List<Tag> tags;
-
-    public Item() {}
-
-    public Item(@NotNull String name, @Nullable String description, double basePrice) {
-        this.name = name;
-        this.description = description;
-        this.basePrice = basePrice;
-    }
-
-    public Item(int id, @NotNull String name, @Nullable String description, double basePrice) {
-        this(name, description, basePrice);
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public @Nullable String getDescription() {
-        return description;
-    }
-
-    public void setDescription(@Nullable String description) {
-        this.description = description;
-    }
-
-    public double getBasePrice() {
-        return basePrice;
-    }
-
-    public void setBasePrice(double basePrice) {
-        this.basePrice = basePrice;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", basePrice=" + basePrice +
-                '}';
-    }
+    @ColumnName("unit") @Nested @Nullable
+    private Unit<?> unit;
+    @Nullable
+    private List<Tag> tags;
 }
