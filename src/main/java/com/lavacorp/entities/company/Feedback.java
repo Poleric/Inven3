b17095166f3b6c9396353f9f3a49a4955be2d86b
HongJunChea <cheahongjun@gmail.com>
2024-07-22 15:06:42 +0800

    feedback

--- src/main/java/com/lavacorp/entities/company/Feedback.java
+++ src/main/java/com/lavacorp/entities/company/Feedback.java
@@ -1,4 +1,11 @@
 package com.lavacorp.entities.company;
 
+import java.util.Date;
+
 public class Feedback {
+    private String feedbackName;
+    private String feedbackDescription;
+    private Date feedbackDate;
+    private int rating;
+    private String feedbackType;
 }
