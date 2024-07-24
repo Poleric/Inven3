e0a378bc1d7be357efaac5f24ef3458b628b6381
Poleric <nixonleongkb@gmail.com>
2024-07-24 14:46:49 +0800

    add Tag.java

--- src/main/java/com/lavacorp/entities/tag/Tag.java
+++ src/main/java/com/lavacorp/entities/tag/Tag.java
@@ -1,5 +1,12 @@
 package com.lavacorp.entities.tag;
 
+import lombok.Data;
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+
+@Data
 public class Tag {
+    @ColumnName("name")
     private String name;
+    @ColumnName("description")
+    private String description;
 }
