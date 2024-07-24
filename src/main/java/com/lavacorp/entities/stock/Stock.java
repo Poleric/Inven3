d12e3a8389588240ca34bb9d115d0e1150c06654
Poleric <nixonleongkb@gmail.com>
2024-07-24 15:21:08 +0800

    add Stock.java

--- src/main/java/com/lavacorp/entities/stock/Stock.java
+++ src/main/java/com/lavacorp/entities/stock/Stock.java
@@ -2,13 +2,21 @@ package com.lavacorp.entities.stock;
 
 import com.lavacorp.entities.company.Supplier;
 import com.lavacorp.entities.item.Item;
+import com.lavacorp.entities.location.Location;
+import lombok.Data;
 import org.jetbrains.annotations.Nullable;
 
-public class Stock {
-    private int batchId;
-    private Item item;
-    private String location;
-    private int quantity;
+import java.time.LocalDateTime;
 
+@Data
+public class Stock {
+    private Item item;
     @Nullable private Supplier supplier;
+    @Nullable private Location location;
+    private int quantity;
+    private StockStatus status;
+    @Nullable private LocalDateTime expiryDate;
+    @Nullable private String notes;
+    @Nullable private LocalDateTime createdAt;
+    @Nullable private LocalDateTime lastUpdatedAt;
 }
