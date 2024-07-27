42755d3a79272fedc7d584121531d9914b88c3a6
Poleric <nixonleongkb@gmail.com>
2024-07-27 09:13:31 +0800

    remove nested

--- src/main/java/com/lavacorp/entities/item/Item.java
+++ src/main/java/com/lavacorp/entities/item/Item.java
@@ -7,7 +7,6 @@ import com.lavacorp.entities.tag.Taggable;
 import com.lavacorp.entities.category.Category;
 
 import lombok.Data;
-import org.jdbi.v3.core.mapper.Nested;
 import org.jetbrains.annotations.Nullable;
 
 import javax.measure.Unit;
@@ -20,8 +19,8 @@ public class Item implements Taggable, Suppliable {
     private String name;
     @Nullable private String description;
     private double basePrice;
-    @Nested @Nullable private Unit<?> unit;
-    @Nested @Nullable private Category category;
+    @Nullable private Unit<?> unit;
+    @Nullable private Category category;
     @Nullable private LocalDateTime createdAt;
     @Nullable private LocalDateTime lastUpdatedAt;
 
