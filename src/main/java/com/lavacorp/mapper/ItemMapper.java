a1a09c220107414e89d03cafdaf68f7282f80423
Poleric <nixonleongkb@gmail.com>
2024-07-29 23:04:48 +0800

    update item mapper

--- src/main/java/com/lavacorp/mapper/ItemMapper.java
+++ src/main/java/com/lavacorp/mapper/ItemMapper.java
@@ -1,21 +1,31 @@
 package com.lavacorp.mapper;
 
+import com.lavacorp.entities.category.Category;
 import com.lavacorp.entities.item.Item;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
 
 public class ItemMapper implements RowMapper<Item> {
 
     @Override
     public Item map(ResultSet rs, StatementContext ctx) throws SQLException {
-        return new Item(
-                rs.getInt("id"),
-                rs.getString("name"),
-                rs.getString("description"),
-                rs.getDouble("base_price")
-        );
+        Item item = new Item(rs.getString("item.name"));
+        item.setId(rs.getObject("item.id", Integer.class));
+        item.setDescription(rs.getString("item.description"));
+        item.setBasePrice(rs.getObject("item.base_price", Double.class));
+        item.setUnit(rs.getString("item.unit"));
+
+        Category category = new Category(rs.getString("category.name"));
+        category.setDescription(rs.getString("category.description"));
+        item.setCategory(category);
+
+        item.setCreatedAt(rs.getObject("item.created_at", Instant.class));
+        item.setLastUpdatedAt(rs.getObject("item.last_updated_at", Instant.class));
+        return item;
     }
 }
+
