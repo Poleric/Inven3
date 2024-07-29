f7a8c2d9de254fa5cece47b6fc3da606b8e124c6
Poleric <nixonleongkb@gmail.com>
2024-07-30 00:10:15 +0800

    create CategoryDao

---
+++ src/main/java/com/lavacorp/db/CategoryDao.java
@@ -0,0 +1,24 @@
+package com.lavacorp.db;
+
+import com.lavacorp.entities.category.Category;
+import org.jdbi.v3.freemarker.UseFreemarkerSqlLocator;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+@UseFreemarkerSqlLocator
+public interface CategoryDao {
+    @SqlUpdate
+    @GetGeneratedKeys("id")
+    int create(@BindBean Category category);
+
+    @SqlQuery
+    Optional<Category> retrieve(int id);
+
+    @SqlQuery
+    Optional<Category> retrieve(@NotNull String name);
+}
