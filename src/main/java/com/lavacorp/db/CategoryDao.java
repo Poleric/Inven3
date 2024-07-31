7cb67e45968a0acea71e956f6b245f6b4b7945ec
Poleric <nixonleongkb@gmail.com>
2024-07-31 15:09:01 +0800

    remove optional

--- src/main/java/com/lavacorp/db/CategoryDao.java
+++ src/main/java/com/lavacorp/db/CategoryDao.java
@@ -12,7 +12,6 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Optional;
 
 @UseFreemarkerSqlLocator
 @AllowUnusedBindings
@@ -23,10 +22,10 @@ public interface CategoryDao {
     int create(@BindBean Category category);
 
     @SqlQuery
-    Optional<Category> retrieve(int id);
+    Category retrieve(int id);
 
     @SqlQuery
-    Optional<Category> retrieve(@NotNull String name);
+    Category retrieve(@NotNull String name);
 
     @SqlQuery("retrieve")
     List<Category> retrieveAll();
