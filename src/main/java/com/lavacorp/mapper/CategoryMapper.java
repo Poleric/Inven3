284605861e01da8842ffc047dff528a9c804c0c9
Poleric <nixonleongkb@gmail.com>
2024-07-31 15:08:08 +0800

    add category mapper

---
+++ src/main/java/com/lavacorp/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.lavacorp.mapper;
+
+import com.lavacorp.entities.category.Category;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryMapper implements RowMapper<Category> {
+    @Override
+    public Category map(ResultSet rs, StatementContext ctx) throws SQLException {
+        Category category = new Category(rs.getString("name"));
+        category.setId(rs.getInt("id"));
+        category.setDescription(rs.getString("description"));
+        return category;
+    }
+}
