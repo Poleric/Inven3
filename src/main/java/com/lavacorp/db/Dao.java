80f15f54e25e82f0ae99ec92b8e81287a4596d34
Poleric <nixonleongkb@gmail.com>
2024-08-02 22:19:17 +0800

    remove @notnull

--- src/main/java/com/lavacorp/db/Dao.java
+++ src/main/java/com/lavacorp/db/Dao.java
@@ -7,7 +7,6 @@ import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
