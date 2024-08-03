b6a5cf04740800fcf86c843ae985f3cb49fdac5a
Poleric <nixonleongkb@gmail.com>
2024-08-03 19:23:16 +0800

    make supplier @Data

--- src/main/java/com/lavacorp/entities/company/Supplier.java
+++ src/main/java/com/lavacorp/entities/company/Supplier.java
@@ -1,5 +1,12 @@
 package com.lavacorp.entities.company;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
 public class Supplier extends Company {
     private String supplyHistory;
 
