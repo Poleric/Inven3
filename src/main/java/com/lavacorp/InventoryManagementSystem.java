9a2e0c6637d9466e29175bea3a13e7fe856f97a9
Poleric <nixonleongkb@gmail.com>
2024-08-08 01:15:54 +0800

    remove Inventory.java

--- src/main/java/com/lavacorp/InventoryManagementSystem.java
+++ src/main/java/com/lavacorp/InventoryManagementSystem.java
@@ -1,11 +1,9 @@
 package com.lavacorp;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class InventoryManagementSystem {
-    private final static Logger LOGGER = LogManager.getLogger(Inventory.class);
-
     public static void main(String[] args) {
     }
 }
