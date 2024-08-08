72db5511de361019bb3175b2b1d5b3c1b4f31b31
Poleric <nixonleongkb@gmail.com>
2024-08-09 00:59:20 +0800

    add increase and decrease stock

--- src/main/java/com/lavacorp/db/dao/StockDao.java
+++ src/main/java/com/lavacorp/db/dao/StockDao.java
@@ -4,6 +4,7 @@ import com.lavacorp.db.dao.generic.Dao;
 import com.lavacorp.entities.Stock;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.List;
 
@@ -14,4 +15,10 @@ public interface StockDao extends Dao<Stock> {
 
     @SqlQuery
     List<Stock> retrieveBySupplierId(int id);
+
+    @SqlUpdate
+    void increaseStock(int id, int quantity);
+
+    @SqlUpdate
+    void decreaseStock(int id, int quantity);
 }
