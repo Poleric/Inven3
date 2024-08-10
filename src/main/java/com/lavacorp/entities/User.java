d0dd60c42264475b32b006820eae9f51977dc3f9
Poleric <nixonleongkb@gmail.com>
2024-08-10 09:05:50 +0800

    add lombok annotation

--- src/main/java/com/lavacorp/entities/User.java
+++ src/main/java/com/lavacorp/entities/User.java
@@ -2,9 +2,14 @@ package com.lavacorp.entities;
 
 import com.lavacorp.entities.generic.DatabaseObj;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
-
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class User extends DatabaseObj {
     private String name;
     private String password;
