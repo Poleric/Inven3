3e2cc4e7ad915e1a5c017753c3d7adf2a64279c9
Poleric <nixonleongkb@gmail.com>
2024-08-24 02:48:14 +0800

    suppress unused warning

--- src/main/java/com/lavacorp/models/User.java
+++ src/main/java/com/lavacorp/models/User.java
@@ -21,6 +21,7 @@ public class User extends NamedDatabaseObj {
         return passwordEncoder.matches(rawPassword, hashedPassword);
     }
 
+    @SuppressWarnings("unused")
     public static abstract class UserBuilder<C extends User, B extends UserBuilder<C, B>> extends NamedDatabaseObj.NamedDatabaseObjBuilder<C, B> {
         public B password(String password) {
             this.hashedPassword = passwordEncoder.encode(password);
