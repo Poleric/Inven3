98d0aa15fbab86638329bb1cdd5c16bd8c69e22d
Poleric <nixonleongkb@gmail.com>
2024-08-24 20:59:15 +0800

    change work factor to 15

--- src/main/java/com/lavacorp/models/User.java
+++ src/main/java/com/lavacorp/models/User.java
@@ -16,7 +16,7 @@ public class User extends NamedDatabaseObj {
     private String hashedPassword;
     private boolean isAdmin;
 
-    public static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    public static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(15);
 
     public boolean comparePassword(String rawPassword) {
         return passwordEncoder.matches(rawPassword, hashedPassword);
