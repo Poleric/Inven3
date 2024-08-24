c3e87a579fcb1e9c65bb426183d933415739810a
Mingyverse <teoh.yongming456@gmail.com>
2024-08-25 02:51:40 +0800

    add methods

--- src/main/java/com/lavacorp/DataManagement.java
+++ src/main/java/com/lavacorp/DataManagement.java
@@ -8,4 +8,29 @@ import com.lavacorp.models.UserType;
 import java.util.Scanner;
 
 public class DataManagement {
+    private static final Scanner scanner =  new Scanner(System.in);
+
+    private static void addData() {
+
+    }
+
+    private static void removeData() {
+
+    }
+
+    private static void updateData() {
+
+    }
+
+    private static void viewData() {
+
+    }
+
+    private static void searchData() {
+
+    }
+
+    private static void filterData() {
+
+    }
 }
