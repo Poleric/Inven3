2ab2c80810e09c11b0fd9d36d7bac43fe8f4acab
Mingyverse <teoh.yongming456@gmail.com>
2024-08-25 02:44:03 +0800

    add imports

--- src/main/java/com/lavacorp/DataManagement.java
+++ src/main/java/com/lavacorp/DataManagement.java
@@ -1,4 +1,11 @@
 package com.lavacorp;
 
+import com.lavacorp.db.Database;
+import com.lavacorp.db.dao.UserDao;
+import com.lavacorp.models.User;
+import com.lavacorp.models.UserType;
+
+import java.util.Scanner;
+
 public class DataManagement {
 }
