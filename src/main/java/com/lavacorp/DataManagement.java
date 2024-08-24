847b22e5de1b3906fa84ce8587dd443142854f2f
Mingyverse <teoh.yongming456@gmail.com>
2024-08-25 02:57:36 +0800

    unfinished tired codes

--- src/main/java/com/lavacorp/DataManagement.java
+++ src/main/java/com/lavacorp/DataManagement.java
@@ -11,7 +11,7 @@ public class DataManagement {
     private static final Scanner scanner =  new Scanner(System.in);
 
     private static void addData() {
-
+        System.out.println("Insert : ");
     }
 
     private static void removeData() {
@@ -27,6 +27,9 @@ public class DataManagement {
     }
 
     private static void searchData() {
+        System.out.print("Search: ");
+        String search = scanner.nextLine();
+
 
     }
 
