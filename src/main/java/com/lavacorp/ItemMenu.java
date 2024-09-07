2c2ba0789056276a636c65a08e902a50dd40dc6e
Mingyverse <teoh.yongming456@gmail.com>
2024-09-07 09:15:22 +0800

    imports part 1

--- src/main/java/com/lavacorp/ItemMenu.java
+++ src/main/java/com/lavacorp/ItemMenu.java
@@ -1,4 +1,14 @@
 package com.lavacorp;
 
+import com.lavacorp.db.Database;
+import com.lavacorp.db.dao.ItemDao;
+import com.lavacorp.models.Item;
+
+
+import java.util.Scanner;
+
 public class ItemMenu {
+    Scanner scanner = new Scanner(System.in);
+
+
 }
