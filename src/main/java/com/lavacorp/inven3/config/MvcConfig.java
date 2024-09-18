7a2609be813825c2675ac37af50ebef7654d6975
Poleric <nixonleongkb@gmail.com>
2024-09-18 22:41:48 +0800

    remove mvc mapping for /home

--- src/main/java/com/lavacorp/inven3/config/MvcConfig.java
+++ src/main/java/com/lavacorp/inven3/config/MvcConfig.java
@@ -13,7 +13,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
 
