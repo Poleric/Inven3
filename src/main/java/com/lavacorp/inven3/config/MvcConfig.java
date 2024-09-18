0e2209b948da474318b66d0b655a54d85713632f
Poleric <nixonleongkb@gmail.com>
2024-09-18 18:38:26 +0800

    mvc mapping

--- src/main/java/com/lavacorp/inven3/config/MvcConfig.java
+++ src/main/java/com/lavacorp/inven3/config/MvcConfig.java
@@ -11,6 +11,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/login").setViewName("login");
-
+        registry.addViewController("/supplier").setViewName("supplier/suppliers");
+        registry.addViewController("/supplier/search").setViewName("supplier/search");
     }
 }
