5810ec2248f8629df5b811c1d889396ad3369aef
Poleric <nixonleongkb@gmail.com>
2024-09-20 23:27:14 +0800

    add stock list

--- src/main/java/com/lavacorp/inven3/controller/StockController.java
+++ src/main/java/com/lavacorp/inven3/controller/StockController.java
@@ -1,9 +1,12 @@
 package com.lavacorp.inven3.controller;
 
 import com.lavacorp.inven3.dao.OrderDirection;
+import com.lavacorp.inven3.dao.PurchaseOrderDao;
 import com.lavacorp.inven3.dao.StockDao;
+import com.lavacorp.inven3.model.PurchaseOrder;
 import com.lavacorp.inven3.model.Stock;
 import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -11,28 +14,52 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 @RequestMapping("/stock")
 public class StockController {
     StockDao stockDao;
+    PurchaseOrderDao purchaseOrderDao;
 
     @Autowired
-    public StockController(StockDao stockDao) {
+    public StockController(StockDao stockDao, PurchaseOrderDao purchaseOrderDao) {
         this.stockDao = stockDao;
+        this.purchaseOrderDao = purchaseOrderDao;
     }
 
     @PostMapping("/search")
     public String show(
+            @RequestParam(name = "query", defaultValue = "") String query,
             @RequestParam(name = "page", defaultValue = "1") int page,
             @RequestParam(name = "page-size", defaultValue = "20") int pageSize,
-            @RequestParam(name = "ordering", defaultValue = "name") String ordering,
+            @RequestParam(name = "ordering", defaultValue = "Stock.id") String ordering,
             @RequestParam(name = "ordering-direction", defaultValue = "ASC") OrderDirection orderingDirection,
             Model model) {
         int totalResults = stockDao.selectAll(true);
+        AtomicInteger totalStocks = new AtomicInteger();
+        AtomicInteger totalStockPurchases = new AtomicInteger();
 
-        List<Stock> results = stockDao.selectAll(ordering, orderingDirection, page, pageSize);
+        List<Stock> stocks = stockDao.selectAllByItemNameLike(query, ordering, orderingDirection, page, pageSize);
+        List<StockContext> results = new ArrayList<>();
+        stocks.forEach(stock -> {
+            assert stock.getId() != null;
+            PurchaseOrder po = purchaseOrderDao.selectByStockId(stock.getId());
+            int purchaseCount = 0;
+            if (po != null)  // this is horrible
+                purchaseCount = po.getStocks().entrySet().stream().dropWhile(
+                        entry -> stock.getId().equals(entry.getKey().getId())
+                ).findFirst().orElse(new AbstractMap.SimpleEntry<>(null, 0)).getValue();
+            results.add(new StockContext(stock, po, purchaseCount));
+            if (stock.getStatus() == Stock.StockStatus.OK
+                || stock.getStatus() == Stock.StockStatus.IN_TRANSIT)
+                totalStocks.addAndGet(stock.getQuantity());
+            totalStockPurchases.addAndGet(purchaseCount);
+        });
+
 
         model.addAttribute("results", results);
         model.addAttribute("pageSize", pageSize);
@@ -40,9 +67,13 @@ public class StockController {
         model.addAttribute("totalPages", totalResults / pageSize + 1);
         model.addAttribute("currentRow", (page - 1) * pageSize + 1);
         model.addAttribute("totalRows", totalResults);
+        model.addAttribute("totalStocks", totalStocks);
+        model.addAttribute("totalStockPurchases", totalStockPurchases);
 
         return "stock/search";
     }
+
+    public record StockContext(Stock stock, @Nullable PurchaseOrder order, int purchaseCount) {}
 
     @PostMapping("/create")
     @ResponseBody
