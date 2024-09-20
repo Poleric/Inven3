750ac0a7c5cda0cebb3afaa922a60949533527a2
Poleric <nixonleongkb@gmail.com>
2024-09-20 20:22:06 +0800

    make item list

--- src/main/java/com/lavacorp/inven3/controller/ItemController.java
+++ src/main/java/com/lavacorp/inven3/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.lavacorp.inven3.controller;
 
 import com.lavacorp.inven3.dao.ItemDao;
 import com.lavacorp.inven3.dao.OrderDirection;
+import com.lavacorp.inven3.dao.StockDao;
 import com.lavacorp.inven3.model.Item;
 import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 @RequestMapping("/item")
 public class ItemController {
     ItemDao itemDao;
+    StockDao stockDao;
 
     @Autowired
-    public ItemController(ItemDao itemDao) {
+    public ItemController(ItemDao itemDao, StockDao stockDao) {
         this.itemDao = itemDao;
+        this.stockDao = stockDao;
     }
 
     @PostMapping("/search")
@@ -28,12 +34,20 @@ public class ItemController {
             @RequestParam(name = "query", defaultValue = "") String query,
             @RequestParam(name = "page", defaultValue = "1") int page,
             @RequestParam(name = "pageSize", defaultValue = "20") int pageSize,
-            @RequestParam(name = "ordering", defaultValue = "name") String ordering,
+            @RequestParam(name = "ordering", defaultValue = "id") String ordering,
             @RequestParam(name = "ordering-direction", defaultValue = "ASC") OrderDirection orderingDirection,
             Model model) {
         int totalResults = itemDao.selectAllByNameLike(query, true);
 
-        List<Item> results = itemDao.selectAllByNameLike(query, ordering, orderingDirection, page, pageSize);
+        List<Item> items = itemDao.selectAllByNameLike(query, ordering, orderingDirection, page, pageSize);
+        AtomicInteger totalStock = new AtomicInteger();
+        Map<Item, Integer> results = new HashMap<>();
+        items.forEach(item -> {
+            assert item.getId() != null;
+            int stockCount = stockDao.countItemStock(item.getId());
+            results.put(item, stockCount);
+            totalStock.addAndGet(stockCount);
+        });
 
         model.addAttribute("results", results);
         model.addAttribute("pageSize", pageSize);
@@ -41,6 +55,7 @@ public class ItemController {
         model.addAttribute("totalPages", totalResults / pageSize + 1);
         model.addAttribute("currentRow", (page - 1) * pageSize + 1);
         model.addAttribute("totalRows", totalResults);
+        model.addAttribute("totalStock", totalStock);
 
         return "item/search";
     }
