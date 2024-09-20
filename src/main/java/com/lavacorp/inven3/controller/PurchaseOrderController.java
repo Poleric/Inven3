f7711fa1b93537c8b4db43b4fd796d4911b5819f
Poleric <nixonleongkb@gmail.com>
2024-09-21 03:35:09 +0800

    po list

--- src/main/java/com/lavacorp/inven3/controller/PurchaseOrderController.java
+++ src/main/java/com/lavacorp/inven3/controller/PurchaseOrderController.java
@@ -1,12 +1,19 @@
 package com.lavacorp.inven3.controller;
 
+import com.lavacorp.inven3.dao.OrderDirection;
 import com.lavacorp.inven3.dao.PurchaseOrderDao;
+import com.lavacorp.inven3.model.PurchaseOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/order/purchase-order")
+@RequestMapping("/purchase")
 public class PurchaseOrderController {
     PurchaseOrderDao purchaseOrderDao;
 
@@ -15,5 +22,21 @@ public class PurchaseOrderController {
         this.purchaseOrderDao = purchaseOrderDao;
     }
 
+    @PostMapping("/search")
+    public String show(
+            @RequestParam(name = "query", defaultValue = "") String query,
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "page-size", defaultValue = "20") int pageSize,
+            @RequestParam(name = "ordering", defaultValue = "Stock.id") String ordering,
+            @RequestParam(name = "ordering-direction", defaultValue = "ASC") OrderDirection orderingDirection,
+            Model model) {
+        int totalResults = purchaseOrderDao.selectAll(true);
 
+        List<PurchaseOrder> pos = purchaseOrderDao.selectAll(ordering, orderingDirection, page, pageSize);
+
+        model.addAttribute("contexts", pos);
+        model.addAttribute("pageContext", new PageContext(pageSize, totalResults, page));
+
+        return "purchase/search";
+    }
 }
