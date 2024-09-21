526dcc4c4477ba58ca371fd104477f39bbc11e86
Poleric <nixonleongkb@gmail.com>
2024-09-21 16:33:30 +0800

    add modal box

--- src/main/resources/static/resources/js/inven3.js
+++ src/main/resources/static/resources/js/inven3.js
@@ -1,6 +1,11 @@
 function cascade_checkbox(source, tag) {
     const form = source.closest("form")
-    const checkboxes = form.querySelectorAll(":scope [name='selected']");
+    const checkboxes = form.querySelectorAll(`:scope ${tag}`);
     for (let i = 0; i < checkboxes.length; i++)
         checkboxes[i].checked = source.checked
+}
+
+function close_modal_box(source) {
+    const modal_box = source.closest(".modal");
+    modal_box.style.display = "none";
 }
