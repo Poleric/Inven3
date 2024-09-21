ac62d7045bf41025ff5d2ed601e222f2c9c76280
Poleric <nixonleongkb@gmail.com>
2024-09-21 18:46:02 +0800

    script for populating data to delete

--- src/main/resources/static/resources/js/inven3.js
+++ src/main/resources/static/resources/js/inven3.js
@@ -9,3 +9,36 @@ function close_modal_box(source) {
     const modal_box = source.closest(".modal");
     modal_box.style.display = "none";
 }
+
+function popup_delete() {
+    const to_be_delete_table = document.querySelector("table.to-delete");
+    to_be_delete_table.innerHTML = "";  // clear
+
+    const table = document.querySelector("table.data-table");
+    const thead = table.querySelector(":scope thead");
+    const tbody = table.querySelector(":scope tbody");
+
+    let rows = [];
+    tbody.querySelectorAll(":scope tr").forEach(
+        (row) => {
+            let checkbox = row.querySelector("[name='selected']");
+            if (checkbox.checked)
+                rows.push(row);
+        }
+    )
+
+    if (rows.length === 0)
+        return;
+
+    to_be_delete_table.append(thead.cloneNode(true));
+    let to_be_delete_table_body = document.createElement("tbody");
+    rows.forEach(
+        (row) => {
+            to_be_delete_table_body.append(row.cloneNode(true));
+        }
+    )
+    to_be_delete_table.append(to_be_delete_table_body);
+
+    document.querySelector('.modal.confirm-delete').style.display = 'flex';
+}
+
